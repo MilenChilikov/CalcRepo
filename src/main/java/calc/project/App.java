850e997a815eaa5735b886379fe4876e422ab1dd
Milen Chilikov <chilikov.milen@gmail.com>
2012-07-24 13:34:44 +0300

    Calc structure commit

--- src/main/java/calc/project/App.java
+++ src/main/java/calc/project/App.java
@@ -1,9 +1,9 @@
 package calc.project;
 
-/**
- * Hello world!
- *
- */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class App 
 {
 	public static boolean  isNumber(char ch){
@@ -52,8 +52,22 @@ public class App
 		return flag;
 	}
 	
-	public static int convertToNumber(String str)	{
-		int num = 0;
+	public static boolean isP1(char ch){
+		if(ch == '(')
+			return true;
+		else 
+			return false;
+	}
+	
+	public static boolean isP2(char ch){
+		if(ch == ')')
+			return true;
+		else 
+			return false;
+	}
+	
+	public static Integer convertToNumber(String str)	{
+		Integer num = 0;
 		int m = 0;
 		
 		for(int i = 0; i < str.length(); ++i)	{
@@ -99,11 +113,53 @@ public class App
 	
     public static void main( String[] args )
     {
-        String str = "*";
+    	Scanner sc = new Scanner(System.in);
+    	
+    	char ch;
+    	int flag = 1, countP = 0, numKey = 1;
+ 
+        String str;
+        Map numbers = new HashMap();
+        Map operations = new HashMap();
         
-        System.out.println(convertToNumber(str));
+        str = sc.next();
+        char oper[] = new char[str.length()/2 + 1];
+        
+        for(int i = 0; i < str.length(); ++i)	{
+        	ch = str.charAt(i);
+        	
+        	if(isNumber(ch) || isOperation(ch) || isParenthesis(ch))	{
+        		if(flag == 1 && isNumber(ch))	{
+        			
+        		}
+        		else if(flag == 1 && isP1(ch))	{
+        			countP++;
+        		}
+        		else if(flag == 0 && isOperation(ch))	{
+        			
+        		}
+        		else if(flag == 0 && isP2(ch))	{
+        			countP--;
+        			if(countP < 0)	{
+        				System.out.println("Invalid input.");
+                		return;
+        			}
+        		}
+        		else {
+        			System.out.println("Invalid input.");
+            		return;
+        		}
+        	}
+        	else	{
+        		System.out.println("Invalid input.");
+        		return;
+        	}
+        }
+        
+        
+        /*System.out.println(convertToNumber(str));
         System.out.println(isNumber(str.charAt(0)));
         System.out.println(isOperation(str.charAt(0)));
-        System.out.println(isParenthesis(str.charAt(0)));
+        System.out.println(isParenthesis(str.charAt(0)));*/
     }
 }
